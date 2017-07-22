59fbcf8278e406e0cd732ff8fca2855be1b29d64
Blasapo <blasapo@gmail.com>
2017-07-22 20:09:09 +0200

    Exercise 1 and 2

---
+++ main.js
@@ -0,0 +1,53 @@
+'use strict'
+
+class Button{
+  constructor(element, text, icon){
+    this.element = document.querySelector(element);
+    this.text = text;
+    this.icon = icon;
+    this.render();
+  }
+  render(){
+    this.element.innerHTML = `
+      ${this.text}
+      <i class="fa fa-${this.icon}"></i>
+    `;
+  }
+}
+const prueba = new Button('.hello-button1','Holi', 'hand-rock-o');
+const prueba2 = new Button('.bye-button1','Adios', 'heart');
+
+// Segundo ejercicio
+// Debemos crear una clase nueva llamada ColorButton que herede de la clase Button.
+// Las diferencias de esta clase con su clase padre (Button) serán las siguientes:
+// Esta clase deberá generar instancias que tengan una propiedad color para poder
+//  modificarla y establecer un color. Ese color será el que posteriormente se utilize
+//  para cambiar el color de fuente del botón.
+// El método render deberá realizar las mismas operaciones que el método render de B
+// utton pero además deberá modificar el estilo del botón para que el texto de este
+// sea del color de la propiedad color:
+
+class redButton extends Button{
+  constructor(element, text, icon){
+    super(element,text, icon);
+      this.element.style.color = "red";
+  }
+}
+const prueba3 = new redButton('.hello-button2','Love music', 'music');
+const prueba4 = new redButton('.bye-button2','Love tree', 'tree');
+
+//Otra frma de hacerlo sería:
+//Aunque habría que revisar los render. Los realiza dos veces
+
+// class RedButton extends Button{
+//   constructor(element, text, icon, color){
+//     super(element,text,icon);
+//     this.color = color;
+//     this.render();
+//   }
+//
+//   render(){
+//     super.render();
+//     this.element.style.color = `${this.color}`;
+//   }
+// }
